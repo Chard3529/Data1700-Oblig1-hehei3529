9a9d57cedc42308a8c99121ddfe5e126aec2239e
Chard3529 <142592975+Chard3529@users.noreply.github.com>
2024-02-12 17:37:09 +0100

    input validation done -8 char limit tlf

--- Oblig1/src/main/resources/static/script.js
+++ Oblig1/src/main/resources/static/script.js
@@ -1,26 +1,139 @@
-let bilettArray = [];
-function velgFilm() {
+const billettArray = [];
+
+//Functions to get information from inputs
+function selectFilm() {
     return $("#film").val();
 }
 
-function buyBilett(){
-    const fornavn = $("#fornavn").val();
-    const etternavn = $("#etternavn").val();
-    const tlf = $("#tlf").val();
-    const epost = $("#epost").val();
+function getAntall() {
+    const antall = $("#antall").val();
+    $("#antall").val("");
+    return antall;
+}
 
-    switch (fornavn) {
-        case "":
-            $("#fornavn").val("Feltet må fylles");
-            break;
+function getPersonalia() {
+    const personalia = {
+        fornavn: $("#fornavn").val(),
+        etternavn: $("#etternavn").val(),
+        tlf: $("#tlf").val(),
+        epost: $("#epost").val()
+    };
 
-        case (!(isNaN(fornavn))):
-            $("#fornavn").val("Skriv in bokstaver");
-            break;
+    $("#fornavn").val("");
+    $("#etternavn").val("");
+    $("#tlf").val("");
+    $("#epost").val("");
 
-        default:
-            $("#fornavn").val(fornavn);
+    return personalia;
+}
+
+//Functions to validate inputs
+
+function isEmptyCheck (a) {
+    if (a === "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function isValidEmail(string) {
+
+    //valid email RegExp taken from (https://www.w3resource.com/javascript/form/email-validation.php)
+    const format =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (string.match(format)) {
+        return true;
+    }
+    else {
+        return false;
     }
 
 }
+
+function clearWarnings(){
+    $("#antallFeil").text("");
+    $("#filmFeil").text("");
+    $("#fornavnFeil").text("");
+    $("#etternavnFeil").text("");
+    $("#tlfFeil").text("");
+    $("#epostFeil").text("");
+}
+
+
+//Function gets inputs, runs checks, and prints errors, or adds information to the array
+function regBillett(){
+    const film = selectFilm();
+    const antall = getAntall();
+    const personalia = getPersonalia();
+    let noFails = true;
+
+    if (film === "NoChoice"){
+        noFails = false;
+        $("#filmFeil").text("Må velge film");
+    }
+
+    //Checks if any of the fields are empty, or contains wrong variable type and prints error
+    if (isEmptyCheck(antall)) {
+        noFails = false;
+        $("#antallFeil").text("Må fylle inn feltet");
+    }
+    else if(isNaN(antall)) {
+        noFails = false;
+        $("#antallFeil").text("Må fylle inn tall");
+    }
+
+    if (isEmptyCheck(personalia.fornavn)) {
+        noFails = false;
+        $("#fornavnFeil").text("Må fylle inn feltet");
+    }
+    else if(!isNaN(personalia.fornavn)) {
+        noFails = false;
+        $("#fornavnFeil").text("Må fylle inn bokstaver");
+    }
+
+    if (isEmptyCheck(personalia.etternavn)) {
+        noFails = false;
+        $("#etternavnFeil").text("Må fylle inn feltet");
+    }
+    else if(!isNaN(personalia.etternavn)) {
+        noFails = false;
+        $("#etternavnFeil").text("Må fylle inn bokstaver");
+    }
+
+    if (isEmptyCheck(personalia.tlf)) {
+        noFails = false;
+        $("#tlfFeil").text("Må fylle inn feltet");
+    }
+    else if(isNaN(personalia.tlf)) {
+        noFails = false;
+        $("#tlfFeil").text("Må fylle inn tall");
+    }
+
+    if (isEmptyCheck(personalia.epost)) {
+        noFails = false;
+        $("#epostFeil").text("Må fylle inn feltet");
+    }
+    else if (!isValidEmail(personalia.epost)) {
+        noFails = false;
+        $("#epostFeil").text("Må fylle inn valid epost");
+    }
+
+    if (noFails) {
+        const billettObject = {
+            film: film,
+            antall: antall,
+            fornavn: personalia.fornavn,
+            etternavn: personalia.etternavn,
+            tlf: personalia.tlf,
+            epost: personalia.epost
+        };
+        billettArray.push(billettObject);
+        console.log(billettObject);
+    }
+
+
+}
+
+
 

ef0b3f97f9c310eb5e37b973f55ca06f899ed6ca
Chard3529 <142592975+Chard3529@users.noreply.github.com>
2024-02-12 10:28:43 +0100

    Commit project files

---
+++ Oblig1/src/main/java/Data1700/Oblig/Oblig1/Oblig1Application.java
@@ -0,0 +1,13 @@
+package Data1700.Oblig.Oblig1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1Application.class, args);
+    }
+
+}
---
+++ Oblig1/src/test/java/Data1700/Oblig/Oblig1/Oblig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Data1700.Oblig.Oblig1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
